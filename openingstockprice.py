3864befdf4244a69d4c0d10a25a6c81844bf53bb
Kru <bavishi.krunal@gmail.com>
2018-02-06 17:21:12 -0500

    added openingstockprice.py

---
+++ openingstockprice.py
@@ -0,0 +1,23 @@
+#Use BeautifulSoup to extract the opening price of Facebook stock from Yahoo Finance.
+
+from urllib.request import urlopen
+import ssl
+from bs4 import BeautifulSoup
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = "https://finance.yahoo.com/quote/FB?p=FB"
+
+html = urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+tag = soup.find('div', id="quote-summary").div.table.tbody
+
+tag1 = tag.findAll('tr')
+tag2 = tag1[1]
+tag3 = tag2.findAll('td')
+tag4 = tag3[1]
+
+print (tag4.span['class'])
